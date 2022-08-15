e2f89dbdd7dfd37a9b3d5daa9bf635c216560398
Natalia Torres <nataliatorres88@gmail.com>
2022-08-15 14:23:31 -0500

    modificacion renderizado

---
+++ js/objeto.js
@@ -0,0 +1,37 @@
+let animal1 = {
+    id:131,
+    nombre: "Pallas Cat",
+    tipo:"Mammal",
+    foto:"https://upload.wikimedia.org/wikipedia/commons/d/d6/Manoel.jpg",
+    tiempoDeVidaEnAños:"8",
+    latin_name:"Octocolobus manul",
+    active_time:"Nocturnal",
+    length_min:"1.5",
+    length_max:"2",
+    weight_min:"6.5",
+    weight_max:"11",
+    habitat:"Mountain regions, including grassland, woodland, and semi-desert",
+    diet:"Pikas, hares, and small rodents such as gerbils, voles, and young marmots",
+    geo_range:"Central Asia",
+    
+    
+}
+
+let animal2 = {
+    id:149,
+    nombre:"Red-eared Slider",
+    foto:"https://upload.wikimedia.org/wikipedia/commons/9/96/RedEaredSlider05.jpg",
+    tipo:"Reptile",
+    tiempoDeVidaEnAños:"20",
+    latin_name:"Trachemys scripta elegans",
+    active_time:"Diurnal",
+    length_min:"0.42",
+    length_max:"0.92",
+    weight_min:"1.5",
+    weight_max:"2",
+    habitat:"Freshwater ponds and streams",
+    diet:"Fish, mollusks, and insects",
+    geo_range:"Southern and Midwestern United States",
+    
+    
+}
