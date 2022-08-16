0a79264c7460167b1835f95311f62ab519bf66d0
Natalia Torres <nataliatorres88@gmail.com>
2022-08-15 19:57:22 -0500

    changes in objeto

--- js/objeto.js
+++ js/objeto.js
@@ -1,37 +1,159 @@
 let animal1 = {
-    id:131,
+    id: 131,
     nombre: "Pallas Cat",
-    tipo:"Mammal",
-    foto:"https://upload.wikimedia.org/wikipedia/commons/d/d6/Manoel.jpg",
-    tiempoDeVidaEnAños:"8",
-    latin_name:"Octocolobus manul",
-    active_time:"Nocturnal",
-    length_min:"1.5",
-    length_max:"2",
-    weight_min:"6.5",
-    weight_max:"11",
-    habitat:"Mountain regions, including grassland, woodland, and semi-desert",
-    diet:"Pikas, hares, and small rodents such as gerbils, voles, and young marmots",
-    geo_range:"Central Asia",
-    
-    
+    tipo: "Mammal",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/d/d6/Manoel.jpg",
+    tiempoDeVidaEnAños: "8",
+    latin_nombre: "Octocolobus manul",
+    active_time: "Nocturnal",
+    habitat: "Mountain regions, including grassland, woodland, and semi-desert",
+    diet: "Pikas, hares, and small rodents such as gerbils, voles, and young marmots",
+    geo_range: "Central Asia",
+
+
 }
 
 let animal2 = {
-    id:149,
-    nombre:"Red-eared Slider",
-    foto:"https://upload.wikimedia.org/wikipedia/commons/9/96/RedEaredSlider05.jpg",
-    tipo:"Reptile",
-    tiempoDeVidaEnAños:"20",
-    latin_name:"Trachemys scripta elegans",
-    active_time:"Diurnal",
-    length_min:"0.42",
-    length_max:"0.92",
-    weight_min:"1.5",
-    weight_max:"2",
-    habitat:"Freshwater ponds and streams",
-    diet:"Fish, mollusks, and insects",
-    geo_range:"Southern and Midwestern United States",
-    
-    
+    id: 149,
+    nombre: "Red-eared Slider",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/9/96/RedEaredSlider05.jpg",
+    tipo: "Reptile",
+    tiempoDeVidaEnAños: "20",
+    latin_nombre: "Trachemys scripta elegans",
+    active_time: "Diurnal",
+    habitat: "Freshwater ponds and streams",
+    diet: "Fish, mollusks, and insects",
+    geo_range: "Southern and Midwestern United States",
+
+
+}
+let animal3 = {
+    nombre: "Bearded Dragon",
+    latin_nombre: "Pogona vitteceps",
+    tipo: "Reptile",
+    active_time: "Diurnal",
+    tiempoDeVidaEnAños: "20",
+    habitat: "Dry, rocky areas and woodland", "diet": "Insects and leaves",
+    geo_range: "Central and Southern Australia",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/5/56/Pogona_vitticeps02.JPG",
+    id: 29
+}
+
+let animal4 = {
+    nombre: "Naked Mole-rat",
+    latin_nombre: "Heterocephalus glaber",
+    tipo: "Mammal",
+    active_time: "Diurnal",
+    tiempoDeVidaEnAños: "30",
+    habitat: "Savannah",
+    diet: "Roots and tubers",
+    geo_range: "Eastern Africa",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/0/02/Nacktmull.jpg",
+    id: 122
+}
+
+let animal5 = {
+    nombre: "Yellow-backed Duiker",
+    latin_nombre: "Cephalophus silvicultor",
+    tipo: "Mammal",
+    active_time: "Diurnal",
+    tiempoDeVidaEnAños: "10",
+    habitat: "Dense tropical forest",
+    diet: "Bark, shoots, buds, seeds, fruits, fungi, herbs and some small animals",
+    geo_range: "Western central Africa",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/4/47/Cephalophus_sylvicultor_sylvicultor2.jpg",
+    id: 196
+}
+
+let animal6 = {
+    nombre: "Vampire Bat",
+    latin_nombre: "Desmodus rotundus",
+    tipo: "Mammal",
+    active_time: "Nocturnal",
+    tiempoDeVidaEnAños: "9",
+    habitat: "Rainforest, scrub, and desert",
+    diet: "Mammal blood",
+    geo_range: "Mexico, Central and South America",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/3/32/Desmodus_rotundus_A_Catenazzi.jpg",
+    id: 180
+}
+
+
+let animal7 = {
+    nombre: "Bald Eagle",
+    latin_nombre: "Haliaeetus leucocephalus",
+    tipo: "Bird",
+    active_time: "Diurnal",
+    tiempoDeVidaEnAños: "16",
+    habitat: "Costal shores, forest, and mountain regions near lakes and rivers",
+    diet: "Fish, small mammals, birds, reptiles, and carrion",
+    geo_range: "North America",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/1/1a/About_to_Launch_%2826075320352%29.jpg",
+    id: 23
+}
+
+let animal8 = {
+    nombre: "Azureus Cichlid",
+    latin_nombre: "Copadichromis azureus",
+    tipo: "Fish",
+    active_time: "Diurnal",
+    tiempoDeVidaEnAños: "8",
+    habitat: "Lakes",
+    diet: "Plankton",
+    geo_range: "Africa",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/0/04/Copadichromis_azureus.jpg",
+    id: 22
+}
+
+let animal9 = {
+    nombre: "Clouded Leopard",
+    latin_nombre: "Neofelis nebulosa",
+    tipo: "Mammal",
+    active_time: "Nocturnal",
+    tiempoDeVidaEnAños: "13",
+    habitat: "Forest, grassland, and wetland",
+    diet: "Primates, deer, wild boar, porcupines, small mammals, and birds",
+    geo_range: "Southeastern Asia",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Clouded_Leopard_Neofelis_nebulosa_by_Dr_Raju_Kasambe_DSC_7497_%2828%29.JPG",
+    id: 55
+}
+
+let animal10 = {
+    nombre: "King Penguin",
+    latin_nombre: "Aptenodytes patagonius",
+    tipo: "Bird",
+    active_time: "Diurnal",
+    tiempoDeVidaEnAños: "25",
+    habitat: "Ocean and islands",
+    diet: "Squid, crustaceans, and fish",
+    geo_range: "Sub-Antarctic islands",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/b/be/SGI-2016-South_Georgia_%28Fortuna_Bay%29%E2%80%93King_penguin_%28Aptenodytes_patagonicus%29_04.jpg",
+    id: 99
+}
+
+let animal11 = {
+    nombre: "Lappet-faced Vulture",
+    latin_nombre: "Torgos tracheliotos",
+    tipo: "Bird",
+    active_time: "Diurnal",
+    tiempoDeVidaEnAños: "50",
+    habitat: "Savannah, semi-desert and desert",
+    diet: "Carrion, small mammals and birds",
+    geo_range: "Africa and Middle East",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/b/b4/2012-lappet-faced-vulture.jpg",
+    id: 102
+}
+
+
+let animal12 = {
+    nombre: "Sunbittern",
+    latin_nombre: "Eurypyga helias",
+    tipo: "Bird",
+    active_time: "Diurnal",
+    tiempoDeVidaEnAños: "15",
+    habitat: "Wetlands",
+    diet: "Insects and other small animals",
+    geo_range: "Southern Mexico to northern South America",
+    foto: "https://upload.wikimedia.org/wikipedia/commons/d/da/Sunbittern_%28Eurypyga_helias%29.JPG",
+    id: 171
 }
