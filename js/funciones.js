8cbb191b22357e74edba917e89462a011675db87
Natalia Torres <nataliatorres88@gmail.com>
2022-08-15 19:56:37 -0500

    changes in funciones

--- js/funciones.js
+++ js/funciones.js
@@ -1,26 +1,46 @@
+let divAnimales = document.querySelector("#cajaAnimales")
 
-
-function plantillaAnimales(objeto) {
-    let nombreCorrecto = objeto.nombre
-
+function plantilla(objeto) {
     return `
-            <div id=${objeto.id} class='datos card'>
-            <p> foto:<img class="card-img-top" src="${objeto.foto} " alt="#"> </p>
-            <p>Nombre: <b>${nombreCorrecto}</b></p>
-            <p>Tipo: <b>${objeto.tipo}</b></p>
-            <p>Tiempo de Vida: <b>${objeto.tiempoDeVidaEnAños} </b></p>
+    <div class="cols">
+    <div id="${objeto.id}" class="col" ontouchstart="this.classList.toggle('hover');">
+        <div class="container">
+            <div class="front">
+                <div class="container">
+                    <img class=" w-100 h-100" src="${objeto.foto} " alt="">
+                </div>
+                <div class="inner">
+                    <p>${objeto.nombre}</p>
+                    <span>${objeto.tipo}</span>
+                </div>
             </div>
+            <div class="back">
+                <div class="inner">
+                    <p>${objeto.latin_nombre}</p>
+                        <p>${objeto.tiempoDeVidaEnAños}</p>
+                        <p>${objeto.active_time}</p>
+                        <p>${objeto.habitat}</p>
+                        <p>${objeto.diet}</p>
+                        <p>${objeto.geo_range}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
     `
 }
-let plantilla = plantillaAnimales(animal1)
+plantilla()
 
 console.log(plantilla)
 
-function impresionEnHtml(idEnHtml, objeto) {
-    let impresion = plantillaAnimales(objeto)
-    let contenedor = document.getElementById(`${idEnHtml}`)
-    contenedor.innerHTML += impresion
-    // let container = document.getElementById(idEnHtml).innerHTML += plantillaAnimales(objeto) // codigo mas corto para hacer la impresion.
+console.log(plantilla)
+
+function impresionEnHtml(cajaAnimales2, objeto) {
+    
+    let impresion = plantilla(objeto)
+    cajaAnimales2.innerHTML += impresion
 }
+impresionEnHtml (divAnimales, cajaAnimales)
 //impresionEnHtml('todos', animal1) //todos es el nombre del div en el html
 //impresionEnHtml('todos', animal2)
